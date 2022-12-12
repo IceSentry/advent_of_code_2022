14d98e00d161aa02e2cc2c830a46a075324acba1
IceSentry <c.giguere42@gmail.com>
2022-12-12 00:52:30 -0500

    day11 done

--- src/day11.rs
+++ src/day11.rs
@@ -35,7 +35,8 @@ fn parse_monkey(block: &str) -> Monkey {
     monkey.operation = scan!("Operation: new = old {} {}" <- line).unwrap();
 
     let line = lines.next().unwrap().trim();
-    monkey.test_value = scan!("Test: divisible by {}" <- line).unwrap();
+    monkey.test_value =
+        scan!("Test: divisible by {}" <- line).unwrap_or_else(|_| panic!("Failed to parse {line}"));
 
     let line = lines.next().unwrap().trim();
     monkey.test_true = scan!("If true: throw to monkey {}" <- line).unwrap();
@@ -50,7 +51,6 @@ pub fn part_1(input: &Data) -> usize {
     let mut monkeys = input.clone();
     for _ in 0..20 {
         for i in 0..input.len() {
-            // println!("Monkey {i}:");
             let monkey = monkeys[i].clone();
 
             if monkey.items.is_empty() {
@@ -59,7 +59,6 @@ pub fn part_1(input: &Data) -> usize {
 
             for item in monkey.items {
                 let mut worry_level = item;
-                // println!("  Monkey inspects an item with a worry level of {worry_level}.");
 
                 let (operator, value) = &monkey.operation;
                 if value != "old" {
@@ -69,57 +68,28 @@ pub fn part_1(input: &Data) -> usize {
                         "+" => worry_level += value,
                         _ => unreachable!(),
                     }
-                    // println!("    Worry level is {operator} by {value} to {worry_level}.")
                 } else {
                     match operator.as_str() {
                         "*" => worry_level *= worry_level,
                         "+" => worry_level += worry_level,
                         _ => unreachable!(),
                     }
-                    // println!("    Worry level is {operator} by itself to {worry_level}.")
                 }
 
                 worry_level /= 3;
-                // println!(
-                //     "    Monkey gets bored with item. Worry level is divided by 3 to {worry_level}."
-                // );
 
                 let throw_index = if worry_level % monkey.test_value == 0 {
-                    // println!(
-                    //     "    Current worry level is divisible by {}.",
-                    //     monkey.test_value
-                    // );
                     monkey.test_true
                 } else {
-                    // println!(
-                    //     "    Current worry level is not divisible by {}.",
-                    //     monkey.test_value
-                    // );
                     monkey.test_false
                 };
-                // println!(
-                //     "    Item with worry level {worry_level} is thrown to monkey {throw_index}."
-                // );
                 monkeys[throw_index].items.push(worry_level);
             }
 
             monkeys[i].inspect_count += monkeys[i].items.len();
             monkeys[i].items.clear();
         }
-
-        // println!();
-        // for m in &monkeys {
-        //     println!("Moneky: {} {:?}", m.index, m.items);
-        // }
     }
-
-    // println!();
-    // for m in &monkeys {
-    //     println!(
-    //         "Moneky {} inspected items {:?} times.",
-    //         m.index, m.inspect_count
-    //     );
-    // }
 
     monkeys.sort_by_key(|m| m.inspect_count);
     monkeys
@@ -131,7 +101,58 @@ pub fn part_1(input: &Data) -> usize {
 }
 
 pub fn part_2(input: &Data) -> usize {
-    0
+    let mut monkeys = input.clone();
+    let modulo: usize = monkeys.iter().map(|m| m.test_value).product();
+
+    for _ in 0..10000 {
+        for i in 0..input.len() {
+            let monkey = monkeys[i].clone();
+
+            if monkey.items.is_empty() {
+                continue;
+            }
+
+            for item in monkey.items {
+                let mut worry_level = item;
+
+                let (operator, value) = &monkey.operation;
+                if value != "old" {
+                    let value = value.parse::<usize>().unwrap();
+                    match operator.as_str() {
+                        "*" => worry_level *= value,
+                        "+" => worry_level += value,
+                        _ => unreachable!(),
+                    }
+                } else {
+                    match operator.as_str() {
+                        "*" => worry_level *= worry_level,
+                        "+" => worry_level += worry_level,
+                        _ => unreachable!(),
+                    }
+                }
+
+                worry_level %= modulo;
+
+                let throw_index = if worry_level % monkey.test_value == 0 {
+                    monkey.test_true
+                } else {
+                    monkey.test_false
+                };
+                monkeys[throw_index].items.push(worry_level);
+            }
+
+            monkeys[i].inspect_count += monkeys[i].items.len();
+            monkeys[i].items.clear();
+        }
+    }
+
+    monkeys.sort_by_key(|m| m.inspect_count);
+    monkeys
+        .iter()
+        .rev()
+        .take(2)
+        .map(|m| m.inspect_count)
+        .product()
 }
 
 #[cfg(test)]
@@ -179,6 +200,6 @@ mod tests {
     pub fn part_2() {
         let input = super::parse(INPUTS);
         let result = super::part_2(&input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 2713310158);
     }
 }
