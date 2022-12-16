c71e047535df911a78b8cfee62f24da425bea3a1
IceSentry <c.giguere42@gmail.com>
2022-12-16 03:25:42 -0500

    day13 done

--- src/day13.rs
+++ src/day13.rs
@@ -6,17 +6,8 @@ type Data = Vec<(Packet, Packet)>;
 pub fn parse(input: &str) -> Data {
     input
         .split("\n\n")
-        .map(|l| {
-            l.split('\n')
-                .map(|l| l.chars().collect::<VecDeque<_>>())
-                .collect::<Vec<_>>()
-        })
-        .map(|pair| {
-            (
-                parse_packet(&mut pair[0].clone())[0].clone(),
-                parse_packet(&mut pair[1].clone())[0].clone(),
-            )
-        })
+        .map(|l| l.lines().map(parse_packet_string).collect::<Vec<_>>())
+        .map(|pair| (pair[0].clone(), pair[1].clone()))
         .collect()
 }
 
@@ -38,6 +29,11 @@ fn packet_string(packet: &Packet) -> String {
         Packet::Int(i) => write!(s, "{i}").unwrap(),
     };
     s
+}
+
+fn parse_packet_string(s: &str) -> Packet {
+    let mut c = s.chars().collect::<VecDeque<_>>();
+    parse_packet(&mut c)[0].clone()
 }
 
 fn parse_packet(raw_packet: &mut VecDeque<char>) -> Vec<Packet> {
@@ -72,74 +68,54 @@ fn parse_packet(raw_packet: &mut VecDeque<char>) -> Vec<Packet> {
 pub fn part_1(input: &Data) -> usize {
     let mut sum = 0;
     for (i, (left, right)) in input.iter().enumerate() {
-        println!("\n== Pair {} ==", i + 1);
-        match compare(left, right, 0) {
-            std::cmp::Ordering::Less => {
-                println!("Left side is smaller, so inputs are in the right order");
-                sum += i + 1;
-            }
-            std::cmp::Ordering::Equal => {}
-            std::cmp::Ordering::Greater => {
-                println!("Right side is smaller, so inputs are not in the right order");
-            }
+        if let std::cmp::Ordering::Less = compare(left, right) {
+            sum += i + 1;
         }
-        println!();
     }
     sum
 }
 
-fn print_depth(depth: usize) {
-    for _ in 0..depth {
-        print!(" ");
-    }
-}
-
-fn compare(left: &Packet, right: &Packet, depth: usize) -> std::cmp::Ordering {
+fn compare(left: &Packet, right: &Packet) -> std::cmp::Ordering {
     use std::cmp::Ordering::*;
     use Packet::*;
-    print_depth(depth);
-    println!(
-        "- Compare: {} vs {}",
-        packet_string(left),
-        packet_string(right)
-    );
     match (left, right) {
         (Int(l), Int(r)) => l.cmp(r),
         (List(l), List(r)) => {
             let mut i = 0;
             while i < l.len() && i < r.len() {
-                let cmp = compare(&l[i], &r[i], depth + 1);
+                let cmp = compare(&l[i], &r[i]);
                 if matches!(cmp, Less | Greater) {
                     return cmp;
                 }
                 i += 1;
             }
-
             l.len().cmp(&r.len())
         }
-        (list, Int(int)) => {
-            let right = List(vec![Int(*int)]);
-            print_depth(depth + 1);
-            println!(
-                "- Mixed types; convert right to {} and retry comparison",
-                packet_string(&right)
-            );
-            compare(list, &right, depth + 1)
-        }
-        (Int(int), list) => {
-            let left = List(vec![Int(*int)]);
-            print_depth(depth + 1);
-            println!(
-                "Mixed types; convert left to {} and retry comparison",
-                packet_string(&left)
-            );
-            compare(&left, list, depth + 1)
-        }
+        (list, Int(int)) => compare(list, &List(vec![Int(*int)])),
+        (Int(int), list) => compare(&List(vec![Int(*int)]), list),
     }
 }
 
 pub fn part_2(input: &Data) -> usize {
-    0
+    let mut packets = vec![];
+    let divider_2 = parse_packet_string("[[2]]");
+    let divider_6 = parse_packet_string("[[6]]");
+    packets.push(divider_2.clone());
+    packets.push(divider_6.clone());
+    for (left, right) in input.clone() {
+        packets.push(left);
+        packets.push(right);
+    }
+    packets.sort_by(compare);
+    let mut product = 0;
+    for (i, p) in packets.iter().enumerate() {
+        if compare(p, &divider_2).is_eq() {
+            product = i + 1;
+        } else if compare(p, &divider_6).is_eq() {
+            product *= i + 1;
+        }
+    }
+    product
 }
 
 #[cfg(test)]
@@ -185,7 +161,7 @@ mod tests {
     pub fn part_2() {
         let input = super::parse(INPUTS);
         let result = super::part_2(&input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 140);
     }
 
     #[test]
