3e49fe8eda8bfce6e5d8bd7b9bca3db16f29b8b1
IceSentry <c.giguere42@gmail.com>
2022-12-19 14:53:33 -0500

    clean up day14

--- src/day14.rs
+++ src/day14.rs
@@ -29,16 +29,10 @@ pub fn parse(input: &str) -> Data {
     (map, max_y)
 }
 
-pub fn part_1((map, max_y): &Data) -> usize {
-    let mut map = map.clone();
-
+fn simulate_sand(map: &HashSet<(usize, usize)>, max_y: usize) -> (usize, usize) {
     let sand_start = (500, 0);
     let mut sand = sand_start;
-    let mut count = 0;
-    loop {
-        if sand.1 > *max_y {
-            break;
-        }
+    while sand.1 <= max_y {
         if !map.contains(&(sand.0, sand.1 + 1)) {
             sand.1 += 1;
         } else if !map.contains(&(sand.0 - 1, sand.1 + 1)) {
@@ -48,59 +42,50 @@ pub fn part_1((map, max_y): &Data) -> usize {
             sand.0 += 1;
             sand.1 += 1;
         } else {
-            map.insert(sand);
-            sand = sand_start;
-            count += 1;
+            break;
         }
+    }
+    sand
+}
+
+pub fn part_1((map, max_y): &Data) -> usize {
+    let mut map = map.clone();
+    let mut count = 0;
+    loop {
+        let sand = simulate_sand(&map, *max_y);
+        if sand.1 > *max_y {
+            break;
+        }
+        map.insert(sand);
+        count += 1;
     }
     count
 }
 
 pub fn part_2((map, max_y): &Data) -> usize {
     let mut map = map.clone();
-    print_map(&map, *max_y);
-
-    let sand_start = (500, 0);
-    let mut sand = sand_start;
     let mut count = 0;
     loop {
-        if !map_contains(&map, *max_y, &(sand.0, sand.1 + 1)) {
-            sand.1 += 1;
-        } else if !map_contains(&map, *max_y, &(sand.0 - 1, sand.1 + 1)) {
-            sand.0 -= 1;
-            sand.1 += 1;
-        } else if !map_contains(&map, *max_y, &(sand.0 + 1, sand.1 + 1)) {
-            sand.0 += 1;
-            sand.1 += 1;
-        } else {
-            count += 1;
-            if sand == sand_start {
-                break;
-            }
-            map.insert(sand);
-            sand = sand_start;
-            // std::thread::sleep(std::time::Duration::from_millis(5));
-            // print_map(&map, *max_y);
+        let sand = simulate_sand(&map, *max_y);
+        map.insert(sand);
+        count += 1;
+        if sand == (500, 0) {
+            break;
         }
     }
     count
 }
 
-fn map_contains(map: &HashSet<(usize, usize)>, max_y: usize, check: &(usize, usize)) -> bool {
-    if !map.contains(check) {
-        check.1 == max_y + 2
-    } else {
-        true
-    }
-}
-
+// std::thread::sleep(std::time::Duration::from_millis(5));
+// print_map(&map, *max_y);
+#[allow(unused)]
 fn print_map(map: &HashSet<(usize, usize)>, max_y: usize) {
     // clear screen and reset cursor at top left
     print!("\x1B[2J\x1B[1;1H");
     println!();
     for y in 0..=max_y + 2 {
         for x in 450..=550 {
-            if map_contains(map, max_y, &(x, y)) {
+            if !map.contains(&(x, y)) {
                 print!("#");
             } else {
                 print!(" ");
